7b4be98f22263617d2fa03e06952ab2ba25d25f6
Logaraja D <logd@paypal.com>
2020-06-08 00:58:15 +0530

    Initial code commit with react, context and hooks

---
+++ src/components/TransactionList.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import Transaction from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
+
+const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((trx) => (
+          <Transaction transaction={trx} key={trx.id} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
---
+++ src/context/AppReducer.js
@@ -0,0 +1,20 @@
+export default (state, action) => {
+  switch (action.type) {
+    case "ADD_TRANSACTION": {
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+    }
+    case "DELETE_TRANSACTION": {
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (trx) => trx.id !== action.payload
+        ),
+      };
+    }
+    default:
+      return state;
+  }
+};
